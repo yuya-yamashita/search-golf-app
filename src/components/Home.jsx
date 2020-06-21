7aab98c0648f7afba8c27e61e304e19ec063f96c
yuya-yamashita <yuya05020@gmail.com>
2020-06-21 14:31:51 +0900

    ローディング処理を追加

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ import addDays from 'date-fns/addDays';
 import axios from 'axios';
 import format from 'date-fns/format';
 import Result from './Result';
+import Loading from './Loading';
 
 const Today = new Date();
 registerLocale('ja', ja);
@@ -20,12 +21,15 @@ class Home extends React.Component {
         duration: '90',
         plans: [],
         planCount: 0,
-        error: null
+        error: null,
+        loading: false
     };
 
     onFormSubmit = async (event) => {
         try {
             event.preventDefault();
+            this.setState({ loading: true })
+
             const response = await axios.get('https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses', {
                 params: {
                     date: format(this.state.date, 'yyyyMMdd'),
@@ -34,7 +38,8 @@ class Home extends React.Component {
                     duration: this.state.duration
                 }
             });
-            this.setState({planCount: response.data.planCount, plans: response.data.plans});
+            this.setState({ planCount: response.data.planCount, plans: response.data.plans });
+            this.setState({ loading: false })
         }
         catch (e) {
             this.setState({ error: e })
@@ -45,19 +50,19 @@ class Home extends React.Component {
         return (
             <div className="ui container" id="container">
                 <div className="Search__Form">
-                    <form className="ui form segment" onSubmit={this.onFormSubmit}>
+                    <form className="ui form segment" onSubmit={ this.onFormSubmit }>
                         <div className="field">
                             <label><i className="calendar alternate outline icon"></i>プレー日</label>
                                 <DatePicker
-                                    dateFormat="yyyy/MM/dd"
-                                    locale='ja'
-                                    selected={this.state.date}
-                                    onChange={e => this.setState({ date: e })}
-                                    minDate={Today}/>
+                                    dateFormat = "yyyy/MM/dd"
+                                    locale     = 'ja'
+                                    selected   = { this.state.date }
+                                    onChange   = { e => this.setState({ date: e }) }
+                                    minDate    = { Today }/>
                         </div>
                         <div className="field">
                             <label><i className="yen sign icon"></i>上限金額</label>
-                            <select className="ui dropdown" name="dropdown" value={this.state.budget} onChange={e => this.setState({ budget: e.target.value })}>
+                            <select className="ui dropdown" name="dropdown" value={ this.state.budget } onChange={e => this.setState({ budget: e.target.value })}>
                                 <option value="8000">8,000円</option>
                                 <option value="12000">12,000円</option>
                                 <option value="16000">16,000円</option>
@@ -65,14 +70,14 @@ class Home extends React.Component {
                         </div>
                         <div className="field">
                             <label><i className="map pin icon"></i>移動時間計算の出発地点（自宅から近い地点をお選びください）</label>
-                            <select className="ui dropdown" name="dropdown" value={this.state.departure} onChange={e => this.setState({ departure: e.target.value })}>
+                            <select className="ui dropdown" name="dropdown" value={ this.state.departure } onChange={e => this.setState({ departure: e.target.value })}>
                                 <option value="1">東京駅</option>
                                 <option value="2">横浜駅</option>
                             </select>
                         </div>
                         <div className="field">
                             <label><i className="car icon"></i>車での移動時間の上限</label>
-                            <select className="ui dropdown" name="dropdown" value={this.state.duration} onChange={e => this.setState({ duration: e.target.value })}>
+                            <select className="ui dropdown" name="dropdown" value={ this.state.duration } onChange={e => this.setState({ duration: e.target.value })}>
                                 <option value="60">60分</option>
                                 <option value="90">90分</option>
                                 <option value="120">120分</option>
@@ -84,10 +89,13 @@ class Home extends React.Component {
                             </button>
                         </div>
                     </form>
+
+                    <Loading loading={ this.state.loading }/>
+
                     <Result
-                        plans={this.state.plans}
-                        planCount={this.state.planCount}
-                        error={this.state.error}
+                        plans     = { this.state.plans }
+                        planCount = { this.state.planCount }
+                        error     = {this.state.error}
                     />
                 </div>
             </div>
